a326e03543e3dfec9aeec140ab42e781ae2016b9
jonathan rubio <jonathanr1156@gmail.com>
2023-10-12 10:01:41 -0700

    first commit

---
+++ chapter 6.py
@@ -0,0 +1,31 @@
+# my_dict = {
+#     1:'Eren',
+#     2: 'Mikasa',
+#     3: 'Armin',
+#     'Reiner': 'Connie',
+#     10.5 : 4
+# }
+
+# my_dict[5] = 'Jean'
+
+# for key in my_dict.keys():
+#     print(key)
+
+
+# print(my_dict)
+
+my_dict = {
+    'football': ['Patriots', 'Seahawks', 'Lions'],
+    'formula one': {'santa', 'elf'},
+    'basketball': {'nba': ['celtics', 'lakers', 'jazz'],
+    'college': ['LSU', 'Florida', 'Alabama']}
+}
+
+print(isinstance(my_dict['basketball']['college'], list))
+
+
+
+
+
+
+
---
+++ chapter 7.py
@@ -0,0 +1,35 @@
+# x = 0
+
+# flag = True
+
+
+# while flag:
+#     print(x)
+#     x += 1
+#     if x > 100:
+#         flag = False
+
+
+while True:
+    my_name = input('Whats your name? ')
+    if my_name.lower() == 'quit':
+        break
+    elif my_name.lower()[0] in 'abcdefgh':
+        print('please proceed to table one')
+    elif my_name.lower()[0] in 'ijklmnopq':
+        print('Please proceed to table two')
+    elif my_name.lower()[0] in 'rstuvwxz':
+        print('Please proceed to table three')
+
+
+
+
+
+
+    
+
+
+
+
+    elif my_name.lower()[0] in 'y':
+        print('MY QUEEEENNNNNN')
